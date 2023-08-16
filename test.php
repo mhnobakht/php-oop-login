28d6d9053e251299971ada47386d2ae1bd2faa6c
mhnobakht <01programmingacademy@gmail.com>
2023-08-16 21:31:23 +0330

    Create Sanitizer Trait

--- test.php
+++ test.php
@@ -6,6 +6,8 @@ use Models\Database;
 
 class User extends Database {
 
+    use Models\SanitizerTrait;
+
     public function get($id) {
         $sql = "SELECT * FROM users WHERE id= ?";
         $stmt = $this->executeStatement($sql, $id);
@@ -13,9 +15,21 @@ class User extends Database {
         return $result->fetch_assoc();
     }
 
+    public function test($input) {
+
+        return self::sanitizeInput($input);
+
+    }
+
 }
 
 
 $userTable = new User();
-$user = $userTable->get([1]);
+
+$input1 = "<script>alert(majid)</script>";
+
+$input2 = ['<script>alert(majid)</script>', 'ali', 'parvane', 'morvarid'];
+
+$user = $userTable->test($input2);
+
 var_dump($user);
