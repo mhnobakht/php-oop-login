0eea5e9a1167a3b424c2faa4344aeea82c670753
mhnobakht <01programmingacademy@gmail.com>
2023-09-04 20:30:33 +0330

    create Mail Class to send Email

--- test.php
+++ test.php
@@ -2,15 +2,12 @@
 
 require_once 'autoload.php';
 
-use Models\Token;
+use Models\Mail;
 
-$user_id = 1;
-$type = 'email';
-$userToken = "e40a474b717e09367d72da5a77a5f5574bcf50f6b1de1ca7501320bd83c28a1b";
+$to = "academy01test@mailna.co";
+$subject = "test subject";
+$message = "click this link to verify your email address";
 
+$result = Mail::send($to, $subject, $message);
 
-$obj = new Token();
-
-$token = $obj->compareToken($user_id, $type, $userToken);
-
-var_dump($token);
+var_dump($result);
